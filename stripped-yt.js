d287abcf5f0f0b63f84d76289da6ccb0fe54f539
johnjago <johnjago@users.noreply.github.com>
2016-07-21 16:46:38 -0500

    Initial commit

---
+++ stripped-yt.js
@@ -0,0 +1,11 @@
+var comments = document.getElementById("watch-discussion");
+comments.outerHTML = "";
+delete element;
+
+var views = document.getElementById("watch7-views-info");
+views.outerHTML = "";
+delete views;
+
+var ratings = document.getElementById("watch8-sentiment-actions");
+ratings.outerHTML = "";
+delete ratings;
