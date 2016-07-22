fa1605c182772ecc2e85164a79fff5ef583dd8f7
johnjago <johnjago@users.noreply.github.com>
2016-07-22 14:22:30 -0500

    Remove extra JS, add watchSubs removal

--- stripped-yt.js
+++ stripped-yt.js
@@ -1,11 +1,11 @@
 var comments = document.getElementById("watch-discussion");
 comments.outerHTML = "";
-delete element;
 
 var views = document.getElementById("watch7-views-info");
 views.outerHTML = "";
-delete views;
 
 var ratings = document.getElementById("watch8-sentiment-actions");
 ratings.outerHTML = "";
-delete ratings;
+
+var watchSubs = document.querySelector(".exp-responsive #content .yt-uix-button-subscription-container .yt-subscriber-count");
+watchSubs.outerHTML = "";
