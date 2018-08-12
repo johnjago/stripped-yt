0fb6c853707f67f287b13b2c3cb905398b17fdb1
John Jago <johnjago@protonmail.com>
2018-08-12 11:45:32 -0500

    Turn off autoplay and annotations

--- stripped-yt.js
+++ stripped-yt.js
@@ -1,27 +1,21 @@
-/**
- * So many parts of YouTube lazy load, so we have to do this the hard way.
- */
+setInterval(disable, 3000);
 
-var clicker = setInterval(switchOffToggles, 1000);
-
-function switchOffToggles() {
-  var settings = document.getElementsByClassName('ytp-button ytp-settings-button')[0];
-  var menu = document.getElementsByClassName('ytp-popup ytp-settings-menu')[0];
-
-  settings.click();
-  menu.style.display = 'none';
-
+function disable() {
   var menuItems = document.getElementsByClassName('ytp-menuitem');
-  console.log(menuItems.length);
 
-  if (menuItems.length >= 4) {
-    for (var i = 0; i < menuItems.length; i++) {
-      var isToggle = menuItems[i].getAttribute('role') === 'menuitemcheckbox';
-      var isEnabled = menuItems[i].getAttribute('aria-checked') === 'true';
-      if (isToggle && isEnabled) {
-        menuItems[i].click();
-      }
-      clearInterval(clicker);
+  // Menu items are not in the DOM until you open the menu
+  if (menuItems.length === 0) {
+    var settings = document.getElementsByClassName('ytp-button ytp-settings-button')[0];
+    settings.click();
+    var menu = document.getElementsByClassName('ytp-popup ytp-settings-menu')[0];
+    menu.style.display = 'none';
+  }
+
+  for (var i = 0; i < menuItems.length; i++) {
+    var isToggle = menuItems[i].getAttribute('role') === 'menuitemcheckbox';
+    var isEnabled = menuItems[i].getAttribute('aria-checked') === 'true';
+    if (isToggle && isEnabled) {
+      menuItems[i].click();
     }
   }
 }
